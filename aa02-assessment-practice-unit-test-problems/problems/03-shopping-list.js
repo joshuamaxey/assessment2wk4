4a85d12a8a0be1a706b185f0cd23e0717de7d1ed
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-06 14:16:41 -0500

    finished problem 3

--- aa02-assessment-practice-unit-test-problems/problems/03-shopping-list.js
+++ aa02-assessment-practice-unit-test-problems/problems/03-shopping-list.js
@@ -19,9 +19,29 @@ console.log(clothesList); // ['tshirt', 'pants']
 
 ***********************************************************************/
 
-function shoppingList(list) {
-  // Your code here 
+function shoppingList(list) { // This function takes in a list (rray) such as groceryList or clothesList
+  return function(item) { // It returns an anonymous function which takes an ITEM from the list as an argument;
+    list.push(item); // The array.push() method PUSHES the item given as an argument to the list provided as an argument
+    return list; // Finally, we return the list with the new element added
+  };
 }
+
+const groceryList = ['eggs'] //* Here we create a "list" (array) called groceryList
+const addToGroceryList = shoppingList(groceryList); // returns a function //* Here, we initialize a new variable addToGroceryList to hold the FUNCTION shoppingList with the "groceryList" array passed in as an argument
+addToGroceryList('maple syrup'); // returns ['eggs', 'maple syrup'] //* Then we call the addToGroceryList function to add the 'maple syrup' element to the 'groceryList' array.
+console.log(groceryList) //* When we log the 'groceryList' array to the console, it now includes the element we added using the addToGroceryList function.
+
+addToGroceryList("brisket");
+console.log(groceryList)
+
+const clothesList = []
+const addToClothesList = shoppingList(clothesList);
+
+addToClothesList('tshirt'); // returns ['tshirt']
+addToClothesList('pants'); // returns ['tshirt', 'pants']
+addToClothesList('shoes');
+
+console.log(clothesList); // ['tshirt', 'pants']
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = shoppingList;
