1d8cb0bb2259d03e6979d4de0033f655bc4f73d3
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-06 15:54:11 -0500

    fixed bug in 05 and finished problem in review

--- aa02-assessment-practice-unit-test-problems/problems/05-filter-user-profiles.js
+++ aa02-assessment-practice-unit-test-problems/problems/05-filter-user-profiles.js
@@ -38,23 +38,37 @@ state.
 ***********************************************************************/
 
 function filterUserProfiles(users, filter) {
-  // Your code here 
+
+  let resultArr = []
+
+  for (let i = 0; i < users.length; i++) {
+
+    let el = users[i];
+    let filterResult = filter(el);
+
+    if (filterResult) {
+      resultArr.push(el.name)
+    }
+
+  }
+  return resultArr;
+
 }
 
 // To test the example, comment out the code below and run the following command
 // in your terminal: node problems/05-filter-user-profiles.js
-// const facebookUsers = [
-//   { name: "John", age: "21", state: "Florida" },
-//   { name: "Mary", age: "57", state: "California" },
-//   { name: "Judy", age: "47", state: "Texas" },
-//   { name: "Mike", age: "32", state: "New York" }
-// ];
+const facebookUsers = [
+  { name: "John", age: "21", state: "Florida" },
+  { name: "Mary", age: "57", state: "California" },
+  { name: "Judy", age: "47", state: "Texas" },
+  { name: "Mike", age: "32", state: "New York" }
+];
 
-// const cb1 = (user) => user.age > 30;
-// console.log(filterUserProfiles(facebookUsers, cb1)); // ["Mary", "Judy", "Mike"]
+const cb1 = (user) => user.age > 30;
+console.log(filterUserProfiles(facebookUsers, cb1)); // ["Mary", "Judy", "Mike"]
 
-// const cb2 = (user) => user.state === "New York";
-// console.log(filterUserProfiles(facebookUsers, cb2)); // ["Mike"]
+const cb2 = (user) => user.state === "New York";
+console.log(filterUserProfiles(facebookUsers, cb2)); // ["Mike"]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
