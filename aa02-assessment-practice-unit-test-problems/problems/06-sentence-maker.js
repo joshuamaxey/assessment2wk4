d034fb17340eee05cb20c9a9cbb1cdcbb89bb006
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-06 16:03:44 -0500

    Finished question 06 in review

--- aa02-assessment-practice-unit-test-problems/problems/06-sentence-maker.js
+++ aa02-assessment-practice-unit-test-problems/problems/06-sentence-maker.js
@@ -11,7 +11,15 @@ sentenceMaker('I', 'love', 'coding'); // 'I love coding!'
 sentenceMaker('The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'); // 'The quick brown fox jumps over the lazy dog!'
 ***********************************************************************/
 
-// Your code here 
+const sentenceMaker = (...args) => {
+
+	return args.join(" ") + "!";
+
+}
+
+console.log(sentenceMaker('Hello', 'World')); // 'Hello World!'
+console.log(sentenceMaker('I', 'love', 'coding')); // 'I love coding!'
+console.log(sentenceMaker('The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog')); // 'The quick brown fox jumps over the lazy dog!'
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
